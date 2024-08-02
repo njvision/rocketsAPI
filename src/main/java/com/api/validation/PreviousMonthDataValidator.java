4ce572e4a19ba36057acd3bccbd403a9282ac607
njvision <natjel1982@gmail.com>
2024-08-02 20:05:42 +0300

    add custom validation

---
+++ src/main/java/com/api/validation/PreviousMonthDataValidator.java
@@ -0,0 +1,28 @@
+package com.api.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class PreviousMonthDataValidator implements ConstraintValidator<PreviousMonthData, String> {
+    @Override
+    public boolean isValid(String dateString, ConstraintValidatorContext constraintValidatorContext) {
+
+        if(dateString == null || dateString.isEmpty()) {
+            return true;
+        }
+
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate date = LocalDate.parse(dateString, formatter);
+            LocalDate now = LocalDate.now();
+            long daysBetween = ChronoUnit.DAYS.between(date, now);
+            return daysBetween >= 0 && daysBetween <= 30;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
